dc442627bd34249824935f78accefd6cc4828d66
NunGaeng <hjmik0805@gmail.com>
2024-11-05 13:23:01 +0900

    Add files via upload

--- day8_array/src/day8_array/ArrayString.java
+++ day8_array/src/day8_array/ArrayString.java
@@ -15,10 +15,6 @@ public class ArrayString {
 		for(int i=0; i<arr.length; i++) {
 			System.out.print("      "+arr[i]+" ");
 		}
-		//예제
-		System.out.println("");
-		String[] strArr = {"프로그래밍","완전","너무","싫어","노는게","좋아요","별로"};
-		System.out.printf("%s, %s %s!", strArr[0], strArr[1], strArr[5]);
 	}
 
 }
---
+++ day8_array/src/day8_array/QuizArray.java
@@ -0,0 +1,11 @@
+package day8_array;
+
+public class QuizArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[] strArr = {"자바","완전","쉬워","어려워","좋아","싫어"};
+		System.out.printf("%s, %s %s \n", strArr[0], strArr[1], strArr[4]);
+	}
+
+}
